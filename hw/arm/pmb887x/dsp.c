9519af223e9f2b71c543efae62a7922eb826b27c
Alula <git@alula.me>
2026-08-17 11:59:07 +0200

    pmb887x: add back stub DSP model

--- hw/arm/pmb887x/dsp.c
+++ hw/arm/pmb887x/dsp.c
@@ -41,6 +41,219 @@
 #define TYPE_PMB887X_DSP	"pmb887x-dsp"
 #define PMB887X_DSP(obj)	OBJECT_CHECK(dsp_state_t, (obj), TYPE_PMB887X_DSP)
 
+// #define STUB_DSP 1
+#ifdef STUB_DSP
+
+typedef struct dsp_state_t dsp_state_t;
+
+struct dsp_state_t {
+	SysBusDevice parent_obj;
+	MemoryRegion mmio;
+	
+	uint32_t unk[2];
+	uint8_t ram[DSP_RAM_SIZE];
+	uint32_t ram0_value;
+	uint32_t revision;
+	uint32_t rom_version;
+	Clock *gsm_clock;
+	qemu_irq mcu_interrupts[PMB887X_DSP_MCU_INT_COUNT];
+	qemu_irq outputs[DSP_OUTPUT_COUNT];
+
+	pmb887x_clc_reg_t clc;
+};
+
+static void dsp_update_state(dsp_state_t *p) {
+	// TODO
+}
+
+static void dsp_reset_input(void *opaque, int id, int level) {
+}
+
+static void dsp_interrupt_input(void *opaque, int id, int level) {
+}
+
+static void dsp_input0(void *opaque, int id, int level) {
+}
+
+static void dsp_input1(void *opaque, int id, int level) {
+}
+
+static void dsp_gsm_input(void *opaque, int signal, int level) {
+}
+
+static uint32_t dsp_ram_read(dsp_state_t *p, uint32_t offset, unsigned size) {
+	uint8_t *data = p->ram;
+	switch (size) {
+		case 1:		return data[offset];
+		case 2:		return data[offset] | (data[offset + 1] << 8);
+		case 4:		return data[offset] | (data[offset + 1] << 8) | (data[offset + 2] << 16) | (data[offset + 3] << 24);
+		default:	abort();
+	}
+	return 0;
+}
+
+static void dsp_ram_write(dsp_state_t *p, uint32_t offset, uint32_t value, unsigned size) {
+	uint8_t *data = p->ram;
+	switch (size) {
+		case 1:
+			data[offset] = value & 0xFF;
+			break;
+		
+		case 2:
+			data[offset] = value & 0xFF;
+			data[offset + 1] = (value >> 8) & 0xFF;
+			break;
+		
+		case 4:
+			data[offset] = value & 0xFF;
+			data[offset + 1] = (value >> 8) & 0xFF;
+			data[offset + 2] = (value >> 16) & 0xFF;
+			data[offset + 3] = (value >> 24) & 0xFF;
+			break;
+
+		default:
+			abort();
+	}
+}
+
+static uint64_t dsp_io_read(void *opaque, hwaddr haddr, unsigned size) {
+	dsp_state_t *p = opaque;
+	
+	uint64_t value = 0;
+
+	switch (haddr) {
+		case DSP_CLC:
+			value = pmb887x_clc_get(&p->clc);
+			break;
+
+		case DSP_ID:
+			value = 0xF022C031;
+			break;
+
+		case DSP_COM_SET:
+			value = p->unk[0];
+			break;
+
+		case DSP_COM_STATUS:
+			value = p->unk[1];
+			break;
+
+		case DSP_RAM0 ... (DSP_RAM0 + DSP_RAM_SIZE):
+			value = dsp_ram_read(p, haddr - DSP_RAM0, size);
+			break;
+
+		default:
+			// IO_DUMP(haddr + p->mmio.addr, size, 0xFFFFFFFF, false);
+			EPRINTF("unknown reg access: %02"PRIX64"\n", haddr);
+			break;
+	}
+
+	IO_DUMP_READ(haddr + p->mmio.addr, size, value);
+	
+	return value;
+}
+
+static void dsp_io_write(void *opaque, hwaddr haddr, uint64_t value, unsigned size) {
+	dsp_state_t *p = opaque;
+	
+	IO_DUMP_WRITE(haddr + p->mmio.addr, size, value);
+
+	switch (haddr) {
+		case DSP_CLC:
+			pmb887x_clc_set(&p->clc, value);
+			break;
+
+		case DSP_COM_SET:
+			p->unk[0] = value;
+			p->unk[1] = 0;
+			break;
+
+		case DSP_COM_STATUS:
+			p->unk[1] = value;
+			break;
+
+		case DSP_RAM0 ... (DSP_RAM0 + DSP_RAM_SIZE):
+			dsp_ram_write(p, haddr - DSP_RAM0, value, size);
+			break;
+
+		default:
+			EPRINTF("unknown reg access: %02"PRIX64"\n", haddr);
+			break;
+	}
+
+	dsp_update_state(p);
+}
+
+static const MemoryRegionOps io_ops = {
+	.read			= dsp_io_read,
+	.write			= dsp_io_write,
+	.endianness		= DEVICE_NATIVE_ENDIAN,
+	.valid			= {
+		.min_access_size	= 1,
+		.max_access_size	= 4
+	}
+};
+
+static void dsp_init(Object *obj) {
+	dsp_state_t *p = PMB887X_DSP(obj);
+	memory_region_init_io(&p->mmio, obj, &io_ops, p, "pmb887x-dsp", DSP_IO_SIZE);
+	sysbus_init_mmio(SYS_BUS_DEVICE(obj), &p->mmio);
+	qdev_init_gpio_in_named(DEVICE(obj), dsp_reset_input, "RESET_IN", 1);
+	qdev_init_gpio_in_named(DEVICE(obj), dsp_interrupt_input, "INT_IN", PMB887X_DSP_INT_COUNT);
+	qdev_init_gpio_out_named(DEVICE(obj), p->mcu_interrupts, "INT_OUT", ARRAY_SIZE(p->mcu_interrupts));
+	qdev_init_gpio_in_named(DEVICE(obj), dsp_input0, "DSPIN0_IN", 1);
+	qdev_init_gpio_in_named(DEVICE(obj), dsp_input1, "DSPIN1_IN", 1);
+	qdev_init_gpio_in_named(DEVICE(obj), dsp_gsm_input, "GSM_IN", PMB887X_DSP_GSM_SIGNAL_COUNT);
+	p->gsm_clock = qdev_init_clock_in(DEVICE(obj), "GSM_CLOCK", NULL, p, 0);
+	qdev_init_gpio_out_named(DEVICE(obj), &p->outputs[0], "DSPOUT0_OUT", 1);
+	qdev_init_gpio_out_named(DEVICE(obj), &p->outputs[1], "DSPOUT1_OUT", 1);
+	qdev_init_gpio_out_named(DEVICE(obj), &p->outputs[2], "DSPOUT2_OUT", 1);
+}
+
+static const Property dsp_properties[] = {
+	DEFINE_PROP_UINT32("ram0_value", dsp_state_t, ram0_value, 0x0801),
+	DEFINE_PROP_UINT32("revision", dsp_state_t, revision, 0),
+	DEFINE_PROP_UINT32("rom_version", dsp_state_t, rom_version, 0),
+};
+
+static void dsp_realize(DeviceState *dev, Error **errp) {
+	dsp_state_t *p = PMB887X_DSP(dev);
+	
+	pmb887x_clc_init(&p->clc);
+	
+	p->unk[0] = 0x01;
+	p->unk[1] = 0x00;
+
+	dsp_ram_write(p, 0, p->ram0_value, 2);
+	dsp_update_state(p);
+}
+
+static void dsp_class_init(ObjectClass *klass, const void *data) {
+	DeviceClass *dc = DEVICE_CLASS(klass);
+	device_class_set_props(dc, dsp_properties);
+	dc->realize = dsp_realize;
+}
+
+void pmb887x_dsp_set_config(DeviceState *dev, const pmb887x_dsp_config_t *config) {
+	dsp_state_t *p = PMB887X_DSP(dev);
+	// p->config = config;
+}
+
+static const TypeInfo dsp_info = {
+    .name          	= TYPE_PMB887X_DSP,
+    .parent        	= TYPE_SYS_BUS_DEVICE,
+    .instance_size 	= sizeof(struct dsp_state_t),
+    .instance_init 	= dsp_init,
+    .class_init    	= dsp_class_init,
+};
+
+static void dsp_register_types(void) {
+	type_register_static(&dsp_info);
+}
+type_init(dsp_register_types)
+
+#else
+
 enum {
 	DSP_BOOT_PLOAD,
 	DSP_BOOT_DLOAD,
@@ -798,3 +1011,5 @@ static void dsp_register_types(void) {
 	type_register_static(&dsp_info);
 }
 type_init(dsp_register_types)
+
+#endif
